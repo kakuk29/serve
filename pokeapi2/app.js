ccc56c7c5620e7b0cf2781094f6ac99d05d3a16d
Damien Martin <90847522+MartinDamien@users.noreply.github.com>
2022-08-25 11:54:34 +0200

    getUniqueId

--- pokeapi2/app.js
+++ pokeapi2/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const favicon = require("serve-favicon");
-let pokemons = require("./mock_pockemon");
 
+let pokemons = require("./mock_pockemon");
 const { success } = require("./helper.js");
+const { getUniqueId } = require("./helper.js");
+
 dotenv.config();
 const app = express();
 
@@ -21,7 +23,6 @@ app
   .use(favicon(__dirname + `/public/images/favicon.ico`))
   .use(morgan("dev"));
 
-
 app.get("/api/pokemon", (req, res) => {
   const pokemon = pokemons;
   const message = pokemon ? `il y a ${pokemons.length} pokemons` : `il n'y a pas de pokemons`;
@@ -31,10 +32,17 @@ app.get("/api/pokemon", (req, res) => {
 app.get("/api/pokemon/:id", (req, res) => {
   const id = req.params.id;
   const pokemon = pokemons.find((p) => p.id == id);
-  const message = pokemon ? `un pokemon a bien été trouver` : `Bruit de cricket`;
+  const message = pokemon ? `un pokemon a bien été trouver`: `Bruit de cricket`;
   res.json(success(message, pokemon));
 });
 
+app.post("/api/pokemon", (req, res) => {
+  const id = getUniqueId(pokemons);
+  const pokemonCreated = { ...req.body, ...{ id: id, created: new Date() } };
+  pokemons.push(pokemonCreated);
+  const message = `le pokemon ${pokemonCreated.name} a bien été créer`;
+  res.json(success(message, pokemonCreated));
+});
 // start the server
 app.listen(app.get("port"), () => {
   console.log(`http://localhost:${app.get("port")}, press Ctrl+C to quit`);
--- pokeapi2/helper.js
+++ pokeapi2/helper.js
@@ -1,3 +1,10 @@
 exports.success = (message, data) => {
   return { message, data };
 };
+
+exports.getUniqueId = (pokemons) => {
+  const pokemonsids = pokemons.map(pokemon => pokemon.id);
+  const maxId = pokemonsids.reduce((a,b) => Math.max(a,b));
+  const uniqueId = maxId + 1;
+  return uniqueId;
+}
