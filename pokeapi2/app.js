77518a264101e7545da4375d852e71385a554468
Damien Martin <90847522+MartinDamien@users.noreply.github.com>
2022-08-25 10:47:03 +0200

    commit

--- pokeapi2/app.js
+++ pokeapi2/app.js
@@ -1,20 +1,35 @@
 const express = require("express");
+let pokemons = require("./mock_pockemon");
 const dotenv = require("dotenv");
+const { success } = require("./helper.js");
 dotenv.config();
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
+// middleware
 
+// app.use((req, res, next) => {
+//   console.log(`URL: ${req.url}`);
+//   next();
+// });
 
-app.get("/", (req, res) => {
-  res.send("Hello node");
+app.get("/api/pokemon", (req, res) => {
+  const pokemon = pokemons;
+  const message = pokemon
+    ? `il y a ${pokemons.length} pokemons`
+    : `il n'y a pas de pokemons`;
+  res.json(success(message, pokemon));
 });
 
 app.get("/api/pokemon/:id", (req, res) => {
-    res.send(`vous avez demandé le pokemon n°${req.params.id}`);
+  const id = req.params.id;
+  const pokemon = pokemons.find((p) => p.id == id);
+  const message = pokemon
+    ? `un pokemon a bien été trouver`
+    : "Bruit de cricket";
+  res.json(success(message, pokemon));
 });
-
 
 // start the server
 app.listen(app.get("port"), () => {
