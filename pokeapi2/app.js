86f94e2b0b5259899fd552197b10a1dc77e3a4ec
Damien Martin <90847522+MartinDamien@users.noreply.github.com>
2022-08-25 11:41:20 +0200

    commit

--- pokeapi2/app.js
+++ pokeapi2/app.js
@@ -18,24 +18,20 @@ app.set("port", process.env.PORT || 3000);
 // });
 
 app
-  .use(favicon(__dirname + "/favicon.ico"))
+  .use(favicon(__dirname + `/public/images/favicon.ico`))
   .use(morgan("dev"));
 
 
 app.get("/api/pokemon", (req, res) => {
   const pokemon = pokemons;
-  const message = pokemon
-    ? `il y a ${pokemons.length} pokemons`
-    : `il n'y a pas de pokemons`;
+  const message = pokemon ? `il y a ${pokemons.length} pokemons` : `il n'y a pas de pokemons`;
   res.json(success(message, pokemon));
 });
 
 app.get("/api/pokemon/:id", (req, res) => {
   const id = req.params.id;
   const pokemon = pokemons.find((p) => p.id == id);
-  const message = pokemon
-    ? `un pokemon a bien été trouver`
-    : "Bruit de cricket";
+  const message = pokemon ? `un pokemon a bien été trouver` : `Bruit de cricket`;
   res.json(success(message, pokemon));
 });
 
